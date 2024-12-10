c4d3626a6193c216bd31d806776e3de18158f400
Bal1oon <ws32159@naver.com>
2024-12-10 17:23:41 +0900

    feat: Comment Entity 생성

--- src/main/java/com/board/repository/CommentRepository.java
+++ src/main/java/com/board/repository/CommentRepository.java
@@ -3,6 +3,8 @@ package com.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.board.entity.Comment;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
