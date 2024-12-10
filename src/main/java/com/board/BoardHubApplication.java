e2caa7aac0f129d01080ac9025b72670210d9ad9
HyesongChae <chg5264@naver.com>
2024-12-10 13:07:24 +0900

    init Project

---
+++ src/main/java/com/board/BoardHubApplication.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardHubApplication.class, args); 
+	}
+
+}
