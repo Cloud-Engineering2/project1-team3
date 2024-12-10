d992eaf0c28ae9889c5a75bc7a078cf3547a26b5
Bal1oon <ws32159@naver.com>
2024-12-10 17:18:23 +0900

    feat: Post Entity 생성

---
+++ src/main/java/com/board/entity/Post.java
@@ -0,0 +1,48 @@
+package com.board.entity;
+
+import com.board.constant.Category;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class Post extends AuditingFields {
+	@Id
+	@Column(name = "pid")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	
+	private String content;
+	
+	private Category category;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "uid")
+	private User user;
+	
+	protected Post() {}
+	
+    private Post(String title, String content, Category category, User user) {
+    	this.title = title;
+    	this.content = content;
+    	this.category = category;
+    	this.user = user;
+    }
+    
+    public static Post of(String title, String content, Category category, User user) {
+    	return new Post(title, content, category, user);
+    }
+}
