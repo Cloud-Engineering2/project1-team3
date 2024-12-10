42445fe79ab2ecf7771efb4793f36e649163c066
KangDaeHyun <jodae00@hanmail.net>
2024-12-10 16:26:11 +0900

    fix: jpa 상속추가

--- src/main/java/com/board/repository/CommentRepository.java
+++ src/main/java/com/board/repository/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.board.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
