d200d850b60f2ee4c130ef9a9ede6f5d6220f88b
Bal1oon <ws32159@naver.com>
2024-12-11 14:02:58 +0900

    feat: 게시글 생성 및 전체 조회 API

--- src/main/java/com/board/controller/PostController.java
+++ src/main/java/com/board/controller/PostController.java
@@ -1,10 +1,31 @@
 package com.board.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.util.List;
 
-@Controller
+import com.board.dto.request.PostRequest;
+import com.board.dto.response.PostResponse;
+import com.board.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
-
+	private final PostService postService;
+	
+	@GetMapping()
+	public ResponseEntity<List<PostResponse>> getPosts() {
+		List<PostResponse> posts = postService.getPosts();
+		return ResponseEntity.ok(posts);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest request) {
+		PostResponse post = postService.createPost(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(post);
+	}
 }
