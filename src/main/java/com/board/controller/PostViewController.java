e540d582e3080e1ae05c6d183174ee79d90267e7
Bal1oon <ws32159@naver.com>
2024-12-13 02:14:03 +0900

    feat: 특정 게시물 조회 페이지 생성

--- src/main/java/com/board/controller/PostViewController.java
+++ src/main/java/com/board/controller/PostViewController.java
@@ -3,11 +3,18 @@ package com.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PostViewController {
     @GetMapping("/posts")
     public String showPosts(Model model) {
         return "post/posts";
+    }
+
+    @GetMapping("/posts/{pid}")
+    public String showPostDetail(@PathVariable Long pid, Model model) {
+        model.addAttribute("postId", pid);
+        return "post/postDetail";
     }
 }
