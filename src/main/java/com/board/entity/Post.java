5ac2d979fb247776b59e241acffa823fdbe21ac7
Bal1oon <ws32159@naver.com>
2024-12-13 02:30:49 +0900

    refactor: 댓글 순서 보장
    - Set -> List 변경으로 댓글 순서 보장
    - 댓글 중복 허용

--- src/main/java/com/board/entity/Post.java
+++ src/main/java/com/board/entity/Post.java
@@ -1,10 +1,9 @@
 package com.board.entity;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import com.board.constant.Category;
 
+import java.util.List;
+import java.util.ArrayList;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -43,7 +43,8 @@ public class Post extends AuditingFields {
 
     @ToString.Exclude
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
-    private final Set<Comment> postComments = new LinkedHashSet<>();
+    @OrderBy("createdAt ASC")
+    private final List<Comment> postComments = new ArrayList<>();
     
 	protected Post() {}
 	
