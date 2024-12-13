acc233a9df3608af770b51275fdd393b6f859032
Bal1oon <ws32159@naver.com>
2024-12-13 10:53:10 +0900

    refactor: 구조 통일

--- src/main/java/com/board/controller/CommentController.java
+++ src/main/java/com/board/controller/CommentController.java
@@ -1,6 +1,9 @@
 package com.board.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.board.common.SuccessResponse;
+import com.board.common.base.BaseResponse;
 import com.board.dto.CommentDto;
+import com.board.dto.CommentDto.CommentRequest;
+import com.board.dto.CommentDto.CommentResponse;
+import com.board.dto.security.PrincipalDetails;
 import com.board.entity.Comment;
 import com.board.service.CommentService;
 
@@ -18,40 +26,80 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/api/posts/{pid}/comments")
 public class CommentController {
 
     private final CommentService commentService;
 
     // 댓글 작성
+//    @PostMapping
+//
+//    public ResponseEntity<CommentDto.CommentResponse> registerComment(
+//            @RequestParam Long postId,
+//            @RequestParam Long userId,
+//            @RequestBody CommentDto.CommentRequest content) {
+//        Comment svaedComment = commentService.registerComment(postId, userId, content);
+//        return ResponseEntity.ok(CommentDto.CommentResponse.from(svaedComment));
+//    }
+    
     @PostMapping
-
-    public ResponseEntity<CommentDto.CommentResponse> registerComment(
-            @RequestParam Long postId,
-            @RequestParam Long userId,
-            @RequestBody CommentDto.CommentRequest content) {
-        Comment svaedComment = commentService.registerComment(postId, userId, content);
-        return ResponseEntity.ok(CommentDto.CommentResponse.from(svaedComment));
+    public ResponseEntity<SuccessResponse<CommentResponse>> registerComment(
+    		@PathVariable Long pid,
+            @RequestBody CommentDto.CommentRequest content
+    ) {
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		Object principal = authentication.getPrincipal();
+		
+		Long currentUid = null;
+		if (principal instanceof PrincipalDetails) {
+			currentUid = ((PrincipalDetails) principal).getUid();
+		}
+		
+        CommentResponse comment = commentService.registerComment(content, pid, currentUid);
+        return new ResponseEntity<>(
+				new SuccessResponse<>(HttpStatus.CREATED.value(), "댓글이 등록되었습니다.", comment),
+				HttpStatus.CREATED
+		);
     }
 
     // 댓글 삭제
     @DeleteMapping("/{commentId}")
-
-    public ResponseEntity<String> deleteComment(
-            @PathVariable Long commentId,
-            @RequestParam Long userId) {
-        commentService.deleteComment(commentId, userId);
-        return ResponseEntity.ok("Comment deleted successfully");
+    public ResponseEntity<BaseResponse> deleteComment(@PathVariable Long commentId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		Object principal = authentication.getPrincipal();
+		
+		Long currentUid = null;
+		if (principal instanceof PrincipalDetails) {
+			currentUid = ((PrincipalDetails) principal).getUid();
+		}
+		
+        commentService.deleteComment(commentId, currentUid);
+        return new ResponseEntity<>(
+				new BaseResponse(HttpStatus.NO_CONTENT.value(), "댓글이 삭제되었습니다."),
+				HttpStatus.NO_CONTENT
+		);
     }
 
     // 댓글 수정
     @PutMapping("/{commentId}")
-
-    public ResponseEntity<CommentDto.CommentResponse> updateComment(
+    public ResponseEntity<SuccessResponse<CommentResponse>> updateComment(
             @PathVariable Long commentId,
-            @RequestParam Long userId,
-            @RequestBody CommentDto.CommentRequest newContent) {
-        Comment updatedComment = commentService.updateComment(commentId, userId, newContent);
-        return ResponseEntity.ok(CommentDto.CommentResponse.from(updatedComment));
+            @RequestBody CommentRequest newContent) {
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		Object principal = authentication.getPrincipal();
+		
+		Long currentUid = null;
+		if (principal instanceof PrincipalDetails) {
+			currentUid = ((PrincipalDetails) principal).getUid();
+		}
+		
+        CommentResponse updatedComment = commentService.updateComment(commentId, currentUid, newContent);
+        return new ResponseEntity<>(
+				new SuccessResponse<>(HttpStatus.CREATED.value(), "댓글이 수정되었습니다.", updatedComment),
+				HttpStatus.CREATED
+		);
     }
 }
--- src/main/java/com/board/service/CommentService.java
+++ src/main/java/com/board/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.board.service;
 
 import com.board.dto.CommentDto;
+import com.board.dto.CommentDto.CommentRequest;
+import com.board.dto.CommentDto.CommentResponse;
 import com.board.entity.Comment;
 import com.board.entity.Post;
 import com.board.entity.User;
+import com.board.exception.PostNotFoundException;
 import com.board.repository.CommentRepository;
 import com.board.repository.PostRepository;
 import com.board.repository.UserRepository;
@@ -20,17 +23,27 @@ public class CommentService {
     private final PostRepository postRepository;
 
     // 댓글 작성
-    public Comment registerComment(Long postId, Long userId, CommentDto.CommentRequest content) {
-    	
-    	// 작성자 조회
-        User user = userRepository.getReferenceById(userId);
-        // 게시글 조회
-        Post post = postRepository.getReferenceById(postId);
-        // 댓글 생성
-        Comment comment = Comment.of(content.getContent(), post, user);
-        // 댓글 저장
-        return commentRepository.save(comment);
-    }
+//    public Comment registerComment(CommentDto.CommentRequest content, Long userId, Long postId) {
+//    	
+//    	// 작성자 조회
+//        User user = userRepository.getReferenceById(userId);
+//        // 게시글 조회
+//        Post post = postRepository.getReferenceById(postId);
+//        // 댓글 생성
+//        Comment comment = Comment.of(content.getContent(), post, user);
+//        // 댓글 저장
+//        return commentRepository.save(comment);
+//    }
+
+    @Transactional
+	public CommentResponse registerComment(CommentRequest request, Long pid, Long uid) {
+		Post post = postRepository.findById(pid).orElseThrow(() -> new PostNotFoundException(pid));
+		User user = userRepository.findById(uid).get();
+		
+		Comment comment = Comment.of(request.getContent(), post, user);
+		commentRepository.save(comment);
+		return CommentResponse.from(comment);
+	}
 
     // 댓글 삭제
     @Transactional
@@ -51,7 +64,7 @@ public class CommentService {
 
     // 댓글 수정
     @Transactional
-    public Comment updateComment(Long commentId, Long userId, CommentDto.CommentRequest newContent) {
+    public CommentResponse updateComment(Long commentId, Long userId, CommentRequest newContent) {
     	
     	// 댓글 조회
         Comment comment = commentRepository.findById(commentId)
@@ -64,7 +77,7 @@ public class CommentService {
 
         // 댓글 내용 수정
         comment.setContent(newContent.getContent());
-        return comment;
+        return CommentResponse.from(comment);
         // 수정 시간은 AuditingFields에서 자동으로 업데이트됨
     }
 
